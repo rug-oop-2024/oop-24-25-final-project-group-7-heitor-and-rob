4adb80c46181c38809440c24857ad0df3877b879
Rob <r.g.l.sligter@student.rug.nl>
2024-11-04 12:21:59 +0100

    working on feature.py

--- autoop/core/ml/feature.py
+++ autoop/core/ml/feature.py
@@ -6,7 +6,29 @@ import numpy as np
 from autoop.core.ml.dataset import Dataset
 
 class Feature(BaseModel):
-    # attributes here
+    def __init__(self, name: str, type: str) -> None:
+        self.name = name
+        self.type = type
 
-    def __str__(self):
-        raise NotImplementedError("To be implemented.")
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @name.setter
+    def name(self, name: str) -> None:
+        if len(name) == 0:
+            raise ValueError("Name must be a non empty string")
+        if isinstance(name, str):
+            self._name = name
+
+    @property
+    def type(self) -> str:
+        return self._type
+
+    @type.setter
+    def type(self, type: str) -> None:
+        if len(type) == 0:
+            raise ValueError("Type must be a non empty string")
+        if type.lower() not in ["numerical","categorical"]:
+            raise ValueError(f"Type must be either numerical or categorical. Recieved {type}")
+        self._type = type.lower()
