66172247c92fb6a33d81e25c769bc43fa0eb39dc
Rob <r.g.l.sligter@student.rug.nl>
2024-11-04 17:56:10 +0100

    own changes. but old

--- autoop/core/ml/artifact.py
+++ autoop/core/ml/artifact.py
@@ -13,12 +13,11 @@ class Artifact(BaseModel, ABC):
     _type: str = PrivateAttr()
     tags: List[str] = Field(default_factory=list)
 
-    def __init__(self, name: str, asset_path: str, version: str, data: bytes, metadata: Dict[str, Any], type: str, tags: Optional[List[str]] = None):
+    def __init__(self, name: str, asset_path: str, version: str, data: bytes, type: str, tags: Optional[List[str]] = None):
         super().__init__(name=name, tags=tags or [])
         self._asset_path = asset_path
         self._version = version
         self._data = data
-        self._metadata = metadata
         self._type = type
         if tags is not None:
             self.tags = tags
@@ -37,7 +36,6 @@ class Artifact(BaseModel, ABC):
             "version": self._version,
             "type": self._type,
             "tags": self.tags,
-            "metadata": self._metadata,
         }
 
     @abstractmethod
