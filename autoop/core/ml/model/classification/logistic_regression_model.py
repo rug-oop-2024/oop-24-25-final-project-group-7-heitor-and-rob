ba7dccfd90be35d5520c03976ccc377665cfbd8c
Heitor <h.brasil.scher.dos.santos@student.rug.nl>
2024-11-10 19:19:48 +0100

    some added functionality to modelling, plus fix to logistic regression and artifact

--- autoop/core/ml/model/classification/logistic_regression_model.py
+++ autoop/core/ml/model/classification/logistic_regression_model.py
@@ -1,30 +1,42 @@
-from sklearn.linear_model import LogisticRegression
+import numpy as np
 from autoop.core.ml.model.model import Model
 
 
 class LogisticRegressionModel(Model):
-    """
-    Logistic Regression Model for classification tasks.
-
-    Attributes:
-        type (str): Type of the model, set to "classification".
-        parameters (dict): Dictionary to store model parameters.
-    """
-
-    def __init__(self, **hyperparameters):
-        """
-        Initialize the LogisticRegressionModel with given hyperparameters.
-
-        Args:
-            hyperparameters (dict): Hyperparameters for the Logistic Regression model.
-        """
+    def __init__(self, learning_rate=0.01, num_iterations=1000, **hyperparameters):
         super().__init__(**hyperparameters)
         self.type = "classification"
+        self.learning_rate = learning_rate
+        self.num_iterations = num_iterations
         self.parameters = {}
-        self.initialize_model()
+        self.weights = None
+        self.bias = None
 
     def initialize_model(self):
-        """
-        Initialize a Scikit-learn LogisticRegression model.
-        """
-        self._model = LogisticRegression(**self._hyperparameters)
+        self.weights = None
+        self.bias = 0
+
+    def sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
+
+    def fit(self, X, y):
+        num_samples, num_features = X.shape
+        self.weights = np.zeros(num_features)
+        self.bias = 0
+
+        for i in range(self.num_iterations):
+            linear_model = np.dot(X, self.weights) + self.bias
+            predictions = self.sigmoid(linear_model)
+
+            dw = (1 / num_samples) * np.dot(X.T, (predictions - y))
+            db = (1 / num_samples) * np.sum(predictions - y)
+
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+        self.parameters = {"weights": self.weights, "bias": self.bias}
+
+    def predict(self, X):
+        linear_model = np.dot(X, self.weights) + self.bias
+        probabilities = self.sigmoid(linear_model)
+        return (probabilities >= 0.5).astype(int)
