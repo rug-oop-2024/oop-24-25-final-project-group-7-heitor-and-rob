729ee8e9a567edb81953ffd7e18843637e00798e
Heitor <h.brasil.scher.dos.santos@student.rug.nl>
2024-11-11 20:26:46 +0100

    potential final commit part 2

--- autoop/core/ml/pipeline.py
+++ autoop/core/ml/pipeline.py
@@ -61,13 +61,13 @@ class Pipeline:
         self._metrics = metrics
         self._artifacts = {}
         self._split = split
-        if target_feature.type == "numerical":
-            if model.type == "classification":
+        if target_feature.type == "categorical":
+            if model.type != "classification":
                 raise ValueError(
                     """Model type must be classification
                   for categorical target feature"""
                 )
-        if target_feature.type == "categorical":
+        if target_feature.type == "numerical":
             if model.type != "regression":
                 raise ValueError(
                     "Model type must be regression "
